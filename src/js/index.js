7400627f8764570051e20da922533b542cc81da1
vinicius-dias3 <vinicius.sod3@gmail.com>
2023-08-28 14:34:12 -0300

    restando readme.md

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,34 @@
-// // const phoneRegex = /^\s\d{4}-\d{4}$/;
-// const padraoSenha = {n}
+data = new Date
+let hora = document.querySelector('#hora')
+let minuto = document.querySelector('#minuto')
+hora.innerHTML = data.getHours()
+minuto.innerHTML = data.getMinutes()
 
-// let senhaConfigurada = prompt('configure uma senha de oito dígitos (apenas números)')
-
-// if(senhaConfigurada && padraoSenha.test(senhaConfigurada)){
-//     console.log('conseguiu testar, aprovado')
-// }else{
-//     console.log('nao foi aprovado no teste')
-// }
-// console.log(senhaConfigurada)
 const senhaUsuario = document.querySelector('#senha-usuario')
 const btnSalvarSenha = document.querySelector('.btn-salvar')
-// console.log(btnSalvarSenha)
+const containerSenha = document.querySelector('.container-senha')
 let tamanhoSenhaUsuario
 let senhaUsuarioGravada
+const numeros = document.querySelectorAll('.numero')
+let numeroDigitado = 0
+let arrayNumeros = []
+let senhaDigitada
+let btnVoltarOuExcluir = document.querySelector('button[value="voltar"]')
+
+function desbloquearTela(){
+    const telaBloqueio = document.querySelector('.tela-bloqueio')
+    telaBloqueio.classList.remove('bloquear')
+    telaBloqueio.classList.add('desbloquear')
+    containerSenha.classList.remove('esconder')
+}
 
 btnSalvarSenha.addEventListener('click', (configurarSenha))
 function configurarSenha(){
-    // let tamanhoSenhaUsuario = senhaUsuario.value.length
-    // let senhaUsuarioGravada = senhaUsuario.value
-    // debugger
-    const containerSenha = document.querySelector('.container-senha')
     const msgErro = document.querySelector('.span')
     if(senhaUsuario.value.length === 8){
         containerSenha.classList.add('esconder')
+        msgErro.classList.remove('mostrar')
+        containerSenha.classList.remove('erro')
         senhaUsuarioGravada = senhaUsuario.value
         tamanhoSenhaUsuario = senhaUsuario.value.length
         console.log(senhaUsuarioGravada)
@@ -32,35 +37,27 @@ function configurarSenha(){
     if(senhaUsuario.value.length < 8 || senhaUsuario.value.length > 8){
         msgErro.classList.add('mostrar-span')
         containerSenha.classList.add('erro')
-        if(senhaUsuario.value.length === 8){
-            msgErro.classList.remove('mostrar')
-            containerSenha.classList.remove('erro')
-            senhaUsuarioGravada = senhaUsuario.value
-            tamanhoSenhaUsuario = senhaUsuario.value.length
-            console.log(senhaUsuarioGravada)
-            console.log(tamanhoSenhaUsuario)
-        }
     }
 }
 
-const numeros = document.querySelectorAll('.numero')
-let senhaDigitada = 0
-let arrayNumeros = []
-
 numeros.forEach((numero)=> {
     numero.addEventListener('click', ()=> {
-        arrayNumeros.push(numero.value)
-        console.log(arrayNumeros)
         let iconeSenhas = document.querySelectorAll('.fa-circle')
-        if(senhaDigitada < iconeSenhas.length){
-            iconeSenhas[senhaDigitada].classList.remove('far')            
-            iconeSenhas[senhaDigitada].classList.add('fas')            
-            senhaDigitada++
-            // console.log(`senha digitada ${senhaDigitada} vezes`)
+        if(arrayNumeros.length < 8){
+            arrayNumeros.push(numero.value)
+            console.log(arrayNumeros)
         }
-        if(senhaDigitada > 0){
-            // debugger
-            let btnVoltarOuExcluir = document.querySelector('.voltar')
+        if(arrayNumeros.length === 8){
+            senhaDigitada = arrayNumeros.join('')
+            console.log(senhaDigitada)
+            testarSenha()
+        }
+        if(numeroDigitado < iconeSenhas.length){
+            iconeSenhas[numeroDigitado].classList.remove('far')            
+            iconeSenhas[numeroDigitado].classList.add('fas')            
+            numeroDigitado++
+        }
+        if(numeroDigitado > 0){
             btnVoltarOuExcluir.innerHTML = 'excluir'
             btnVoltarOuExcluir.addEventListener('click', excluirSenha)
         }
@@ -69,33 +66,33 @@ numeros.forEach((numero)=> {
     
 function excluirSenha(){
     let iconeSenhas = document.querySelectorAll('.fa-circle')
-    let btnVoltarOuExcluir = document.querySelector('.voltar')
-    // debugger
-    if (senhaDigitada > 0) {
-        senhaDigitada--
-        // console.log(`btnVoltarOuExcluir clicado. senhaDigitada:${senhaDigitada}`)
-        iconeSenhas[senhaDigitada].classList.remove('fas')
-        iconeSenhas[senhaDigitada].classList.add('far')
+        numeroDigitado--
+        iconeSenhas[numeroDigitado].classList.remove('fas')
+        iconeSenhas[numeroDigitado].classList.add('far')
         arrayNumeros.pop()
         console.log(arrayNumeros)
-    }
-    if(senhaDigitada === 0){
-        // console.log(`senha digitada chegou em 0`)
+
+    if(numeroDigitado === 0){
         btnVoltarOuExcluir.removeEventListener('click', excluirSenha)
         btnVoltarOuExcluir.innerHTML = 'voltar'
     }
 }
 
-// function testarSenha(){
-//     let iconeSenhas = document.querySelectorAll('.fa-circle')
-//     // if(tamanhoSenhaUsuario == iconeSenhas.length && senhaUsuarioGravada == )
-
-
-//     if(senhaDigitada.length === iconeSenhas.length && senhaUsuario.value === senhaDigitada){
-//         console.log('A senha está correta')
-//     }else{
-//         console.log('A senha digitada está incorreta. Tente novamente')
-//     }
-// }
-// let tamanhoSenhaUsuario
-// let senhaUsuarioGravada
+function testarSenha(){
+    let smartphone = document.querySelector('.smartphone')
+    const tela = document.querySelector('.tela')
+    let msgErro = document.querySelector('.msg-erro')
+    if(senhaUsuarioGravada === senhaDigitada){
+        smartphone.classList.remove('animacao-erro-senha')
+        msgErro.classList.remove('mostrar-msg')
+        msgErro.classList.add('esconder-msg')
+        tela.classList.add('desbloqueada')
+        while(tela.firstChild){
+            tela.removeChild(tela.firstChild)
+        }
+    }else{
+        smartphone.classList.add('animacao-erro-senha')
+        msgErro.classList.remove('esconder-msg')
+        msgErro.classList.add('mostrar-msg')
+    }
+}
